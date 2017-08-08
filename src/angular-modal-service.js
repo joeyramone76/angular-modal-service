2e1cbbc91cbedad1fcedf18689a7b82fea66612b
Umberto Miceli <miceli.umb@gmail.com>
2017-08-08 10:53:49 +0200

    Create an option parameter to Fix Modal Closes when using bootstrap tabs inside modal

--- src/angular-modal-service.js
+++ src/angular-modal-service.js
@@ -75,7 +75,7 @@ module.factory('ModalService', ['$animate', '$document', '$compile', '$controlle
 
           //  Create a new scope for the modal.
           var modalScope = (options.scope || $rootScope).$new();
-          var rootScopeOnClose = $rootScope.$on('$locationChangeSuccess', cleanUpClose);
+          var rootScopeOnClose = $rootScope.$on('$locationChangeSuccess', options.notCloseOnLocationChangeSuccess ? null : cleanUpClose);
 
           //  Create the inputs object to the controller - this will include
           //  the scope, as well as all inputs provided.
